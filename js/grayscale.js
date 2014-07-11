307f2f04f00d0354f0617c21e896c7d21f09360e
andtheWings <riggins.daniel@zoho.com>
2014-07-11 13:01:37 -0400

    140711 wiki update

--- js/grayscale.js
+++ js/grayscale.js
@@ -21,7 +21,7 @@ $(function() {
 //Google Map Skin - Get more at http://snazzymaps.com/
 var myOptions = {
     zoom: 15,
-    center: new google.maps.LatLng(42.337477, -83.073015),
+    center: new google.maps.LatLng(53.385873, -1.471471),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
     styles: [{
